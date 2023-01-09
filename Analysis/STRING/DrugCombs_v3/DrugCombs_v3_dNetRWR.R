6f031b06bb82abc6f71aa9c07acecf92ddcdf3ef
ag1805x <ag1805x@outlook.in>
2023-01-09 12:37:45 +0200

    RWR

---
+++ Analysis/STRING/DrugCombs_v3/DrugCombs_v3_dNetRWR.R
@@ -0,0 +1,123 @@
+set.seed(5081)
+rm(list = ls())
+
+
+
+# dNET RWR on Drug Combinations (version 3)
+
+
+
+# Load libraries
+library(optparse)
+library(foreach)
+library(doParallel)
+source("/research/groups/fortino/arindam/DrugCombination_1/Scripts/Functions/Functions_dNet_RWR_analysis.R")
+source("/research/groups/fortino/arindam/DrugCombination_1/Scripts/Functions/Functions_parallelprocesses.R")
+
+
+
+
+
+# Get arguments
+option_list = list(
+  make_option(c("--disease"), type = "character", default = NULL, 
+              help = "Name of the disease. The disease name will also be used as file name. e.g.: LungCancer, BreastCancer, etc.", metavar = "character")
+)
+
+opt_parser = OptionParser(option_list = option_list)
+opt = parse_args(opt_parser)
+
+if(is.null(opt$disease)){
+  print_help(opt_parser)
+  stop("--disease argument needed", call.=FALSE)
+}
+
+# Define global options for this script 
+disease <- opt$disease
+
+cat(paste0("\n\nExecuting for: ", disease, "\n\n"))
+
+
+
+# Read the drug combinations
+drugCombs <- readRDS(paste0("InputFiles/DrugCombinations/DrugCombs_v3/DrugComb_", disease, "_v3.rds"))
+cat("\n\nNumber of drug combinations:\n")
+lapply(drugCombs, nrow)
+
+
+
+# Read drug target interactions
+drug_target_ixn <- readRDS("InputFiles/Associations/DrugBank_Drug_Target_Net.rds")
+
+
+
+# Read the network on which to run RWR
+rwr_input_network <- readRDS("InputFiles/Networks/STRING_PPI_Net_database.rds")
+
+
+
+
+# Execute RWR
+nproc <- detectCores()/2 #Check the number of cores. Use only half of available
+cl <- makeCluster(nproc)
+registerDoParallel(cl) 
+
+
+drugCombs_rwr_res1 <- foreach (i=1:nrow(drugCombs$effectiveCombinations),
+                               .packages = c("igraph", "tidyr", "dnet")) %dopar% {
+# drugCombs_rwr_res1 <- for(i in 1:nrow(drugCombs$effectiveCombinations)){
+                                 tmp <- func_dNetRWR_on_drugCombination(rwr_input_network = rwr_input_network,
+                                                                        drug1 = drugCombs$effectiveCombinations[i,c("Drug1_DrugBank_drug_id")], 
+                                                                        drug2 = drugCombs$effectiveCombinations[i,c("Drug2_DrugBank_drug_id")],
+                                                                        drug_target_ixn = drug_target_ixn,
+                                                                        rwr_norm_input = "laplacian",
+                                                                        rwr_norm_output = "none", 
+                                                                        rwr_restart = 0.5)
+                                 tmp
+                               }
+
+for(i in 1:length(drugCombs_rwr_res1)){
+  names(drugCombs_rwr_res1)[i] <- paste("Eff", drugCombs$effectiveCombinations[i,c("Drug1_DrugBank_drug_id")], 
+                                        drugCombs$effectiveCombinations[i,c("Drug2_DrugBank_drug_id")], sep = "__")
+}
+
+
+
+drugCombs_rwr_res2 <- foreach (i=1:nrow(drugCombs$adverseCombinations),
+                               .packages = c("igraph", "tidyr", "dnet")) %dopar% {
+# drugCombs_rwr_res2 <- for(i in 1:nrow(drugCombs$adverseCombinations)){
+                                 tmp <- func_dNetRWR_on_drugCombination(rwr_input_network = rwr_input_network,
+                                                                        drug1 = drugCombs$adverseCombinations[i,c("Drug1_DrugBank_drug_id")], 
+                                                                        drug2 = drugCombs$adverseCombinations[i,c("Drug2_DrugBank_drug_id")],
+                                                                        drug_target_ixn = drug_target_ixn,
+                                                                        rwr_norm_input = "laplacian",
+                                                                        rwr_norm_output = "none", 
+                                                                        rwr_restart = 0.5)
+                                 tmp
+                               }
+
+for(i in 1:length(drugCombs_rwr_res2)){
+  names(drugCombs_rwr_res2)[i] <- paste("Eff", drugCombs$adverseCombinations[i,c("Drug1_DrugBank_drug_id")], 
+                                        drugCombs$adverseCombinations[i,c("Drug2_DrugBank_drug_id")], sep = "__")
+}
+
+
+stopCluster(cl)
+unregister_dopar()
+
+
+
+
+
+# Export results
+drugCombs_rwr_res_final <- list()
+drugCombs_rwr_res_final$effectiveCombinations <- drugCombs_rwr_res1
+drugCombs_rwr_res_final$adverseCombinations <- drugCombs_rwr_res2
+
+if(!dir.exists(paste0("Analysis/STRING/DrugCombs_v3/", disease, "/"))){
+  dir.create(paste0("Analysis/STRING/DrugCombs_v3/", disease, "/"), recursive = TRUE)
+} 
+
+save(drugCombs_rwr_res_final, file = paste0("Analysis/STRING/DrugCombs_v3/", disease, "/dNetRWR050_", disease, "_DrugCombinations.rda"))
+
+print(warnings())
--- Scripts/Functions/Functions_dNet_RWR_analysis.R
+++ Scripts/Functions/Functions_dNet_RWR_analysis.R
@@ -11,7 +11,7 @@ library(dnet)
 
 
 
-# Function to create seed matrix for RWR from drug target interactions
+# Function to create seed matrix for RWR from drug target interactions ---------
 # 
 # 
 # Test:
@@ -37,8 +37,8 @@ func_RWR_seed_from_DTI <- function(rwr_input_network, drugs, drug_target_ixn){
   
   # Extract targets for the selected drugs from the DTI 
   drug_target_ixn <- drug_target_ixn[drug_target_ixn$Node1_drugbank_drug_id %in% drugs, ]
-  
-  
+  drug_target_ixn <- as.data.frame(sapply(drug_target_ixn, as.vector))
+
   # Filter to keep targets in the network
   drug_target_ixn <- drug_target_ixn[drug_target_ixn$Node2_ensembl_gene_id %in% V(rwr_input_network)$name, ]
   
@@ -62,18 +62,69 @@ func_RWR_seed_from_DTI <- function(rwr_input_network, drugs, drug_target_ixn){
 
 
 
+# Function for running dNet RWR on drug pairs ----------------------------------
+# 
+# 
+# Test: 
+# drugCombs <- readRDS(paste0("InputFiles/DrugCombinations/DrugCombs_v3/DrugComb_", disease, "_v3.rds"))
+# drug1 = drugCombs$effectiveCombinations[23, "Drug1_DrugBank_drug_id"]
+# drug2 = drugCombs$effectiveCombinations[23, "Drug2_DrugBank_drug_id"]
+# rwr_input_network <- readRDS("InputFiles/Networks/STRING_PPI_Net_database.rds")
+# drug_target_ixn <- readRDS("InputFiles/Associations/DrugBank_Drug_Target_Net.rds")
+# rwr_restart = 0.5
+# rwr_norm_input = "none"
+# rwr_norm_output = "none"
+# verbose = TRUE
 
 
 
+func_dNetRWR_on_drugCombination <- function(rwr_input_network, drug1, drug2,
+                                            drug_target_ixn,
+                                            rwr_restart = 0.5,
+                                            rwr_norm_input = "none",
+                                            rwr_norm_output = "none",
+                                            verbose = TRUE){
+  
+  print(paste("RWR input network size (nodes, edges):", vcount(rwr_input_network), ecount(rwr_input_network)))
+  
+  # Extract the seed matrix 
+  rwr_input_seed <- func_RWR_seed_from_DTI(rwr_input_network = rwr_input_network, 
+                                           drugs = c(drug1, drug2), 
+                                           drug_target_ixn = drug_target_ixn)
+  
+  union_seed <- ifelse(rowSums(rwr_input_seed) > 0, 1, 0)
+  intersect_seed <- ifelse(rowSums(rwr_input_seed) == 2, 1, 0)
+  rwr_input_seed <- cbind(rwr_input_seed, union_seed, intersect_seed)
 
-
-
-
-
-
-
-
-
-
+  # Execute dNet RWR
+  rwr_result <- suppressMessages(dRWR(g = rwr_input_network, 
+                                      setSeeds = rwr_input_seed, 
+                                      normalise = rwr_norm_input, 
+                                      restart = rwr_restart, 
+                                      normalise.affinity.matrix = rwr_norm_output))
+  
+  # Prepare result for export
+  rwr_result <- as.data.frame(as.matrix(rwr_result))
+  colnames(rwr_result) <- colnames(rwr_input_seed)
+  rwr_result$node_name <- V(rwr_input_network)$name
+  rwr_result$is_union_seed <- as.logical(as.data.frame(rwr_input_seed)$union_seed[match(rwr_result$node_name, row.names(rwr_input_seed))])
+  
+  rwr_result$addEffect <- rowSums(rwr_result[,c(drug1, drug2)])
+  rwr_result <- rwr_result[order(rwr_result$union_seed, decreasing = TRUE), ]
+  rwr_result <- rwr_result[, c("node_name", "is_union_seed",drug1, drug2, "addEffect", "union_seed", "intersect_seed")]
+  row.names(rwr_result) <- NULL
+  
+  
+  if(verbose){
+    cat("Quantile-affinities 95%) \n")
+    cat(" - with Drug1(", drug1, ") :", quantile(rwr_result[,colnames(rwr_result) == drug1], 0.95), "\n")
+    cat(" - with Drug2(", drug2, ") :", quantile(rwr_result[,colnames(rwr_result) == drug2], 0.95), "\n")
+    cat(" - with Drug1 + Drug2:", quantile(rwr_result$addEffect, 0.95), "\n")
+    cat(" - with Drug1 or Drug2:", quantile(rwr_result$union_seed, 0.95), "\n")
+    cat(" - with Drug1 & Drug2:", quantile(rwr_result$intersect_seed, 0.95), "\n")
+  }
+  
+  return(rwr_result)
+}
 
 
