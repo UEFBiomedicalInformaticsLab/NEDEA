849b2a4b89247849a98fbb3c16c99529982e908f
ag1805x <ag1805x@outlook.in>
2023-01-12 16:32:44 +0200

    FIMM drug combinations

---
+++ Scripts/Reference_lists/FIMMDrugComb_drugCombinations.R
@@ -0,0 +1,156 @@
+set.seed(5081)
+rm(list = ls())
+
+
+
+# Drug combinations from FIMM DrugComb (https://drugcomb.fimm.fi/)
+
+
+
+library(httr)
+library(jsonlite)
+httr::set_config(config(ssl_verifypeer = FALSE, ssl_verifyhost = FALSE))
+
+# Download drug combinations from FIMM DrugComb 
+if(!dir.exists("Databases/FimmDrugComb/"))dir.create("Databases/FimmDrugComb/")
+if(!file.exists("Databases/FimmDrugComb/summary_v_1_5.csv")){
+  download.file(url = "https://drugcomb.fimm.fi/jing/summary_v_1_5.csv",
+                destfile = "Databases/FimmDrugComb/summary_v_1_5.csv", method = "wget")
+}
+
+# Read the DrugComb data
+FimmDrugComb_drugCombCat <- read.csv("Databases/FimmDrugComb/summary_v_1_5.csv", header = TRUE)
+FimmDrugComb_drugCombCat <- FimmDrugComb_drugCombCat[FimmDrugComb_drugCombCat$drug_row != "NULL", ]
+FimmDrugComb_drugCombCat <- FimmDrugComb_drugCombCat[FimmDrugComb_drugCombCat$drug_col != "NULL", ]
+
+
+# Map drugs to DrugBank drug ID
+FimmDrugComb_drugs <- GET("https://api.drugcomb.org/drugs")
+FimmDrugComb_drugs <- fromJSON(rawToChar(FimmDrugComb_drugs$content))
+
+
+FimmDrugComb_drugCombCat$Drug1_DrugBank_drug_id <- FimmDrugComb_drugs$drugbank_id[match(FimmDrugComb_drugCombCat$drug_row, FimmDrugComb_drugs$dname)]
+FimmDrugComb_drugCombCat$Drug2_DrugBank_drug_id <- FimmDrugComb_drugs$drugbank_id[match(FimmDrugComb_drugCombCat$drug_col, FimmDrugComb_drugs$dname)]
+
+
+FimmDrugComb_drugCombCat <- FimmDrugComb_drugCombCat[(FimmDrugComb_drugCombCat$Drug1_DrugBank_drug_id != "NA"),]
+FimmDrugComb_drugCombCat <- FimmDrugComb_drugCombCat[(FimmDrugComb_drugCombCat$Drug2_DrugBank_drug_id != "NA"),]
+
+
+
+# Assign categories based on the scores
+# Positive scores infer synergism and negative scores infer antagonism
+# For our study selecting only drug pairs voted synegism/antagonism in all four score types
+FimmDrugComb_drugCombCat$synergy_zip_class <- ifelse(FimmDrugComb_drugCombCat$synergy_zip > 0, 1, ifelse(FimmDrugComb_drugCombCat$synergy_zip < 0, -1, 0))
+FimmDrugComb_drugCombCat$synergy_loewe_class <- ifelse(FimmDrugComb_drugCombCat$synergy_loewe > 0, 1, ifelse(FimmDrugComb_drugCombCat$synergy_loewe < 0, -1, 0))
+FimmDrugComb_drugCombCat$synergy_hsa_class <- ifelse(FimmDrugComb_drugCombCat$synergy_hsa > 0, 1, ifelse(FimmDrugComb_drugCombCat$synergy_hsa < 0, -1, 0))
+FimmDrugComb_drugCombCat$synergy_bliss_class <- ifelse(FimmDrugComb_drugCombCat$synergy_bliss > 0, 1, ifelse(FimmDrugComb_drugCombCat$synergy_bliss < 0, -1, 0))
+
+FimmDrugComb_drugCombCat$synergy_class_sum <- rowSums(FimmDrugComb_drugCombCat[, 
+                                                      c("synergy_zip_class", "synergy_loewe_class", 
+                                                        "synergy_hsa_class", "synergy_bliss_class")], na.rm = TRUE)
+
+FimmDrugComb_drugCombCat <- FimmDrugComb_drugCombCat[abs(FimmDrugComb_drugCombCat$synergy_class_sum) == 4, ]
+FimmDrugComb_drugCombCat$drug_class <- NULL
+FimmDrugComb_drugCombCat[FimmDrugComb_drugCombCat$synergy_class_sum == 4, "drug_class"] <- "synergy"
+FimmDrugComb_drugCombCat[FimmDrugComb_drugCombCat$synergy_class_sum == -4, "drug_class"] <- "antagonism"
+
+
+FimmDrugComb_drugCombCat <- split(FimmDrugComb_drugCombCat, f = FimmDrugComb_drugCombCat$tissue_name)
+FimmDrugComb_drugCombCat <- lapply(FimmDrugComb_drugCombCat, function(x){split(x, f = x$drug_class)})
+
+
+saveRDS(FimmDrugComb_drugCombCat, "InputFiles/ReferenceList/FimmDrugComb_drugCombinations.rds")
+
+
+# FimmDrugComb_drugCombCat$lung$synergy$study_name
+
+
+
+
+# Count number of combinations
+tmp <- lapply(FimmDrugComb_drugCombCat, function(x)lapply(x, function(x){nrow(unique(x[, c("Drug1_DrugBank_drug_id", "Drug2_DrugBank_drug_id")]))}))
+tmp <- do.call(rbind, tmp)
+write.csv(tmp, "Databases/FimmDrugComb/FimmDrugComb_drug_categories_by_tissue.csv")
+
+
+
+
+
+# LungCancer specific
+tmp <- FimmDrugComb_drugCombCat$lung
+tmp <- lapply(tmp, function(x){
+  x <- unique(x[, c("Drug1_DrugBank_drug_id", "Drug2_DrugBank_drug_id")])
+  row.names(x) <- NULL
+  x
+  })
+saveRDS(tmp, "InputFiles/ReferenceList/FimmDrugComb_LungCancer_drugCombinations.rds")
+
+
+
+
+
+# BreastCancer specific
+tmp <- FimmDrugComb_drugCombCat$breast
+tmp <- lapply(tmp, function(x){
+  x <- unique(x[, c("Drug1_DrugBank_drug_id", "Drug2_DrugBank_drug_id")])
+  row.names(x) <- NULL
+  x
+})
+saveRDS(tmp, "InputFiles/ReferenceList/FimmDrugComb_BreastCancer_drugCombinations.rds")
+
+
+
+
+
+# KidneyCancer specific
+tmp <- FimmDrugComb_drugCombCat$kidney
+tmp <- lapply(tmp, function(x){
+  x <- unique(x[, c("Drug1_DrugBank_drug_id", "Drug2_DrugBank_drug_id")])
+  row.names(x) <- NULL
+  x
+})
+saveRDS(tmp, "InputFiles/ReferenceList/FimmDrugComb_KidneyCancer_drugCombinations.rds")
+
+
+
+
+
+# OvaryCancer specific
+tmp <- FimmDrugComb_drugCombCat$ovary
+tmp <- lapply(tmp, function(x){
+  x <- unique(x[, c("Drug1_DrugBank_drug_id", "Drug2_DrugBank_drug_id")])
+  row.names(x) <- NULL
+  x
+})
+saveRDS(tmp, "InputFiles/ReferenceList/FimmDrugComb_OvaryCancer_drugCombinations.rds")
+
+
+
+
+
+# ProstateCancer specific
+tmp <- FimmDrugComb_drugCombCat$prostate
+tmp <- lapply(tmp, function(x){
+  x <- unique(x[, c("Drug1_DrugBank_drug_id", "Drug2_DrugBank_drug_id")])
+  row.names(x) <- NULL
+  x
+})
+saveRDS(tmp, "InputFiles/ReferenceList/FimmDrugComb_ProstateCancer_drugCombinations.rds")
+
+
+
+
+
+# SkinCancer specific
+tmp <- FimmDrugComb_drugCombCat$skin
+tmp <- lapply(tmp, function(x){
+  x <- unique(x[, c("Drug1_DrugBank_drug_id", "Drug2_DrugBank_drug_id")])
+  row.names(x) <- NULL
+  x
+})
+saveRDS(tmp, "InputFiles/ReferenceList/FimmDrugComb_SkinCancer_drugCombinations.rds")
+
+
+
+print(warnings())
---
+++ Scripts/Reference_lists/FIMMDrugComb_drug_annotation.R
@@ -0,0 +1,132 @@
+set.seed(5081)
+rm(list = ls())
+
+
+
+# Drug combinations from FIMM DrugComb (https://drugcomb.fimm.fi/)
+
+
+
+
+
+# Load libraries
+library(unixtools)
+source("/research/groups/fortino/arindam/DrugCombination_1/Scripts/Functions/Functions_ID_Conversion.R")
+
+
+
+# Set temporary directory
+if(!dir.exists("tmp_dir/")){dir.create("tmp_dir/", recursive = TRUE)}
+set.tempdir("tmp_dir/")
+
+
+
+
+
+# Download drug combinations from FIMM DrugComb 
+if(!dir.exists("Databases/FimmDrugComb/"))dir.create("Databases/FimmDrugComb/")
+if(!file.exists("Databases/FimmDrugComb/summary_v_1_5.csv")){
+  download.file(url = "https://drugcomb.fimm.fi/jing/summary_v_1_5.csv",
+                destfile = "Databases/FimmDrugComb/summary_v_1_5.csv", method = "wget")
+}
+
+# Read the DrugComb data
+FimmDrugComb_drugCombCat <- read.csv("Databases/FimmDrugComb/summary_v_1_5.csv", header = TRUE)
+
+
+# Extract list of participating drugs
+FimmDrugComb_drugs <- sort(unique(c(FimmDrugComb_drugCombCat$drug_row, FimmDrugComb_drugCombCat$drug_col))) #4622
+
+
+FimmDrugComb_drugs_byStudy <-list()
+for(study in sort(unique(FimmDrugComb_drugCombCat$study_name))){
+  tmp <- FimmDrugComb_drugCombCat[FimmDrugComb_drugCombCat$study_name == study, ]
+  FimmDrugComb_drugs_byStudy[[study]] <- sort(unique(c(tmp$drug_row, tmp$drug_col)))
+}
+
+
+# sort(table(substr(FimmDrugComb_drugs, 1, 3)), decreasing = TRUE)
+
+
+
+# Check if drug names have been previously mapped. 
+# Saves time in retrieving
+if(file.exists("InputFiles/ReferenceList/FimmDrugComb_drugs_idMap.rds")){
+  FimmDrugComb_drugs_idMap_old <- readRDS("InputFiles/ReferenceList/FimmDrugComb_drugs_idMap.rds")
+  FimmDrugComb_drugs <- FimmDrugComb_drugs[FimmDrugComb_drugs %in% FimmDrugComb_drugs_idMap$drug_names]
+}else{FimmDrugComb_drugs_idMap_old <- data.frame()}
+
+
+
+
+
+# Map using DrugBank supplied identifiers
+DrugBank_Drugs_idMap <- read.csv("Databases/DrugBank/drug_external_identifiers.csv", header = TRUE)
+map1 <- unique(DrugBank_Drugs_idMap[DrugBank_Drugs_idMap$identifier %in% FimmDrugComb_drugs,c("identifier", "parent_key")])
+colnames(map1) <- c("drug_names", "DrugBank_drug_id")
+row.names(map1) <- NULL
+FimmDrugComb_drugs <- FimmDrugComb_drugs[!FimmDrugComb_drugs %in% map1$drug_names]
+
+
+
+
+
+# Map using metaboliteIDmapping R package
+library(metaboliteIDmapping)
+tmp <- metabolitesMapping[grep(pattern = "^DB[0-9]+$", x = metabolitesMapping$Drugbank), ]
+map2 <- as.data.frame(unique(tmp[tmp$Name %in% FimmDrugComb_drugs, c("Name", "Drugbank")]))
+colnames(map2) <- c("drug_names", "DrugBank_drug_id")
+row.names(map2) <- NULL
+FimmDrugComb_drugs <- FimmDrugComb_drugs[!FimmDrugComb_drugs %in% map2$drug_names]
+
+
+tmp <- metabolitesMapping[grep(pattern = "^DB[0-9]+$", x = metabolitesMapping$Drugbank), ]
+map3 <- as.data.frame(unique(tmp[tmp$CAS %in% FimmDrugComb_drugs, c("CAS", "Drugbank")]))
+colnames(map3) <- c("drug_names", "DrugBank_drug_id")
+row.names(map3) <- NULL
+FimmDrugComb_drugs <- FimmDrugComb_drugs[!FimmDrugComb_drugs %in% map3$drug_names]
+rm(tmp)
+
+
+
+
+
+# Map using WebChem
+library(webchem)
+
+# names_2_cas <- cir_query(identifier = FimmDrugComb_drugs[1:20], representation = "cas", match = "all", verbose = TRUE)
+# names_2_cas <- func_list_2_df(names_2_cas)
+# names_2_cas <- names_2_cas[!names_2_cas$V2 == "NA", ]
+# colnames(names_2_cas) <- c("drug_name", "cas")
+# saveRDS(names_2_cas, "tmp_dir/names_2_cas.rds")
+
+
+if(file.exists("tmp_dir/names_2_cas.rds")){
+  names_2_cas <- readRDS("tmp_dir/names_2_cas.rds")
+}else{names_2_cas <- data.frame()}
+FimmDrugComb_drugs <- FimmDrugComb_drugs[!FimmDrugComb_drugs %in% names_2_cas$drug_name]
+names_2_cas_new <- cir_query(identifier = FimmDrugComb_drugs, representation = "cas", match = "all", verbose = TRUE)
+names_2_cas_new <- func_list_2_df(names_2_cas_new)
+names_2_cas_new <- names_2_cas_new[!names_2_cas_new$V2 == "NA", ]
+colnames(names_2_cas_new) <- c("drug_name", "cas")
+names_2_cas <- rbind(names_2_cas, names_2_cas_new)
+saveRDS(names_2_cas, "tmp_dir/names_2_cas.rds")
+
+
+#Map the retrieved CAS to DrugBank drug ID
+DrugBank_Drugs <- read.csv("Databases/DrugBank/drug.csv", header = TRUE)
+DrugBank_Drugs <- DrugBank_Drugs[DrugBank_Drugs$cas_number %in% names_2_cas$cas, c("cas_number", "primary_key")]
+map4 <- merge(names_2_cas, DrugBank_Drugs, by.x = "cas", by.y = "cas_number")
+map4 <- unique(map4[, c("drug_name", "primary_key")])
+colnames(map4) <- c("drug_names", "DrugBank_drug_id")
+row.names(map4) <- NULL
+
+
+FimmDrugComb_drugs_idMap <- unique(rbind(map1, map2, map3, map4))
+FimmDrugComb_drugs_idMap <- rbind(FimmDrugComb_drugs_idMap_old, FimmDrugComb_drugs_idMap)
+row.names(FimmDrugComb_drugs_idMap) <- NULL
+
+saveRDS(FimmDrugComb_drugs_idMap, "InputFiles/ReferenceList/FimmDrugComb_drugs_idMap.rds")
+
+
+print(warnings())
