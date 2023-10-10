85d9787c54b3134a2b883f4f7460b6ef0c16726d
ag1805x <ag1805x@outlook.in>
2023-10-10 12:04:40 +0300

    Drug target extraction

---
+++ Scripts/Drug_combinations/Extract_drug_targets.R
@@ -0,0 +1,176 @@
+set.seed(5081)
+
+
+
+
+# Script to extract known drug targets from DrugBank and extend them using various databases
+
+
+
+# Load libraries
+library(optparse)
+library(igraph)
+library(tidyverse)
+library(org.Hs.eg.db)
+library(OmnipathR)
+library(readxl)
+source("Scripts/Functions/Functions_drug_target.R")
+
+
+
+
+# Get arguments
+option_list = list(
+  make_option(c("--disease"), type = "character", default = NULL, 
+              help = "Name of the disease. The disease name will also be used as file name. e.g.: LungCancer, BreastCancer, etc.", metavar = "character")
+)
+
+opt_parser = OptionParser(option_list = option_list)
+opt = parse_args(opt_parser)
+
+if(is.null(opt$disease)){
+  print_help(opt_parser)
+  stop("--disease argument needed", call.=FALSE)
+}
+
+
+# Define global options for this script 
+disease <- opt$disease
+
+cat(paste0("\n\nExecuting for: ", disease, "\n\n"))
+
+
+
+
+
+# disease <- "BreastCancer"
+
+
+
+# Read the drug combinations
+drugCombs <- readRDS("InputFiles/DrugCombinations/DrugCombinations.rds")
+drugCombs <- drugCombs[[disease]]
+cat(paste0("\n\nNumber of drug combinations: ", nrow(drugCombs), "\n\n"))
+
+
+# Read the network
+input_network <- readRDS("InputFiles/Networks/STRING_PPI_Net_database.rds")
+cat(paste0("\n\nInput network size:: vertices = ", vcount(input_network), ", edges = ", ecount(input_network), "\n\n"))
+
+
+# Read drug target interactions
+drug_target_ixn <- readRDS("InputFiles/Associations/DrugBank_Drug_Target_Net.rds")
+cat("\n\nReading drug-target interactions")
+cat(paste0("\n\tNumber of drug-target interactions: ", nrow(drug_target_ixn)))
+cat(paste0("\n\tNumber of drugs: ", length(unique(drug_target_ixn$Node1_drugbank_drug_id))))
+cat(paste0("\n\tNumber of targets: ", length(unique(drug_target_ixn$Node2_ensembl_gene_id))))
+
+
+# Keep drug targets that are included in the input network
+drug_target_ixn <- drug_target_ixn %>% 
+  filter(Node2_ensembl_gene_id %in% V(input_network)$name) 
+cat("\nFiltering drug-target interactions to keep targets in the network")
+cat(paste0("\n\tNumber of drug-target interactions: ", nrow(drug_target_ixn)))
+cat(paste0("\n\tNumber of drugs: ", length(unique(drug_target_ixn$Node1_drugbank_drug_id))))
+cat(paste0("\n\tNumber of targets: ", length(unique(drug_target_ixn$Node2_ensembl_gene_id))))
+
+
+# Aggregate the drug targets as a single string
+drugTarget_list <- drug_target_ixn %>%
+  group_by(Node1_drugbank_drug_id) %>%
+  summarise(drugTarget_ensembl_id = paste(Node2_ensembl_gene_id, collapse = ","))
+
+
+# Merge the drug targets information with the drug combinations data
+drugCombs_targets <- drugCombs %>%
+  left_join(drugTarget_list, by = c("Drug1_DrugBank_id" = "Node1_drugbank_drug_id")) %>%
+  dplyr::rename(drugTarget_ensembl_id_1 = drugTarget_ensembl_id) %>%
+  left_join(drugTarget_list, by = c("Drug2_DrugBank_id" = "Node1_drugbank_drug_id")) %>%
+  dplyr::rename(drugTarget_ensembl_id_2 = drugTarget_ensembl_id) %>%
+  filter(!is.na(drugTarget_ensembl_id_1), !is.na(drugTarget_ensembl_id_2)) %>%  # Remove rows where either drug has zero targets
+  dplyr::rowwise() %>%
+  dplyr::mutate(
+    drugTarget_ensembl_id = list(unique(unlist(strsplit(c(drugTarget_ensembl_id_1, drugTarget_ensembl_id_2), ",")))),
+    drugTarget_count = length(unlist(drugTarget_ensembl_id))
+  ) %>%
+  dplyr::select(-drugTarget_ensembl_id_1, -drugTarget_ensembl_id_2) 
+
+
+# Simplify the target column as comma separated string
+drugCombs_targets$drugTarget_ensembl_id <- sapply(drugCombs_targets$drugTarget_ensembl_id, function(x) paste(x, collapse = ","))
+
+
+# Convert Ensembl IDs of the targets to gene symbols
+drugCombs_targets$drugTarget_geneSymbol <- sapply(drugCombs_targets$drugTarget_ensembl_id, convert_ensembl_to_symbol, USE.NAMES = FALSE)
+
+
+# Extract known gene/protein interactions from various databases 
+ixns_PS <- import_omnipath_interactions(resources = c("PhosphoSite","phosphoELM","PhosphoNetworks","PhosphoSite_MIMP",
+                                                      "PhosphoSite_ProtMapper","phosphoELM_MIMP","PhosphoPoint",
+                                                      "PhosphoSite_KEA","PhosphoSite_noref"))
+ixns_SIGNOR <- import_omnipath_interactions(resources = c("SIGNOR","SIGNOR_ProtMapper","SIGNOR_CollecTRI"))
+ixns_NPA <- import_omnipath_interactions(resources = c("NetPath"))
+ixns_RIs <- import_transcriptional_interactions()
+
+
+# Extend the drug targets using Phospho
+pho_result_list <- lapply(drugCombs_targets$drugTarget_geneSymbol, function(x) extend_targets_ixns_database(x, ixns_PS))
+drugCombs_targets$ext_pho_targets <- sapply(pho_result_list, function(x) x$ext_targets)
+drugCombs_targets$ext_pho_tar_cnt <- sapply(pho_result_list, function(x) x$ext_tar_cnt)
+
+
+# Extend the drug targets using SIGNOR
+signor_result_list <- lapply(drugCombs_targets$drugTarget_geneSymbol, function(x) extend_targets_ixns_database(x, ixns_SIGNOR))
+drugCombs_targets$ext_sig_targets <- sapply(signor_result_list, function(x) x$ext_targets)
+drugCombs_targets$ext_sig_tar_cnt <- sapply(signor_result_list, function(x) x$ext_tar_cnt)
+
+
+# Extend the drug targets using NetPath
+npa_result_list <- lapply(drugCombs_targets$drugTarget_geneSymbol, function(x) extend_targets_ixns_database(x, ixns_NPA))
+drugCombs_targets$ext_npa_targets <- sapply(npa_result_list, function(x) x$ext_targets)
+drugCombs_targets$ext_npa_tar_cnt <- sapply(npa_result_list, function(x) x$ext_tar_cnt)
+
+
+# Extend the drug targets using RNs
+reg_result_list <- lapply(drugCombs_targets$drugTarget_geneSymbol, function(x) extend_targets_ixns_database(x, ixns_RIs))
+drugCombs_targets$ext_reg_targets <- sapply(reg_result_list, function(x) x$ext_targets)
+drugCombs_targets$ext_reg_tar_cnt <- sapply(reg_result_list, function(x) x$ext_tar_cnt)
+
+
+
+# Read the list of KEGG pathways linked to Cancer hallmarks
+if(!dir.exists("Databases/CHG/")){dir.create("Databases/CHG/", recursive = TRUE)}
+if(!file.exists("Databases/CHG/Table_1.xls")){
+  download.file(url = "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7013921/bin/Table_1.xls",
+                destfile = "Databases/CHG/Table_1.xls", method = "wget")
+}
+
+CHG_pathways <- read_excel("Databases/CHG/Table_1.xls", skip = 2)
+CHG_pathways <- CHG_pathways$Pathway
+CHG_pathways <- sort(unique(unlist(str_split(CHG_pathways, "\n"))))
+CHG_pathways <- CHG_pathways[CHG_pathways != ""]
+
+
+# # Extract the gene symbols for all the known targets in drug-target interactions
+# all_drug_target_gene_symbols <- select(org.Hs.eg.db, 
+#                                        keys = unique(drug_target_ixn$Node2_ensembl_gene_id), 
+#                                        columns = "SYMBOL", 
+#                                        keytype = "ENSEMBL")
+
+# Download protein interactions from KEGG
+ixns_KEGG <- download_KEGG_ixns(pathway_ids = CHG_pathways)
+
+
+# Extend the drug targets using interactions from KEGG 
+kegg_result_list <- lapply(drugCombs_targets$drugTarget_geneSymbol, function(x) extend_targets_kegg_list(x, ixns_KEGG))
+drugCombs_targets$ext_kegg_targets <- sapply(kegg_result_list, function(x) x$ext_kegg_targets)
+drugCombs_targets$ext_kegg_tar_cnt <- sapply(kegg_result_list, function(x) x$ext_kegg_tar_cnt)
+
+
+
+if(!dir.exists("InputFiles/Drug_targets/")){dir.create("InputFiles/Drug_targets/", recursive = TRUE)}
+saveRDS(drugCombs_targets, file = paste0("InputFiles/Drug_targets/Drug_targets_extended_", disease, ".rds"))
+
+
+
+print(warnings())
---
+++ Scripts/Functions/Functions_drug_target.R
@@ -0,0 +1,127 @@
+# Functions used in extracting and extending drug targets
+
+
+
+# Function to convert Ensembl IDs to gene symbol
+convert_ensembl_to_symbol <- function(ids_str) {
+  ids <- unlist(strsplit(ids_str, ","))
+  suppressMessages(mapping <- select(org.Hs.eg.db, 
+                                     keys = ids, 
+                                     columns = "SYMBOL", 
+                                     keytype = "ENSEMBL"))
+  return(paste(mapping$SYMBOL, collapse = ","))
+}
+
+
+
+# Function to convert gene symbol to Ensembl IDs
+convert_symbol_to_ensembl <- function(ids_str) {
+  ids <- unlist(strsplit(ids_str, ","))
+  suppressMessages(mapping <- select(org.Hs.eg.db, keys = ids, 
+                                     columns = "ENSEMBL", 
+                                     keytype = "SYMBOL"))
+  return(paste(mapping_df$ensembl_gene_id, collapse = ","))
+}
+
+
+
+# Function to extend known drug targets with any 'interaction' database
+extend_targets_ixns_database <- function(ids_str, ixns_data) {
+  known_drug_targets <- unlist(strsplit(ids_str, ","))
+  # Extend drug targets using the provided interaction database
+  extended_targets <- ixns_data[ixns_data$source_genesymbol %in% known_drug_targets,]
+  extended_targets <- extended_targets[which(extended_targets$curation_effort > 1 & extended_targets$is_directed),]
+  all_targets <- unique(c(extended_targets$source_genesymbol, extended_targets$target_genesymbol, known_drug_targets))
+  return(list("ext_targets" = paste(all_targets, collapse = ","), 
+              "ext_tar_cnt" = length(all_targets)))
+}
+
+
+
+# Function to extend drug targets by using a kegg pathway
+extend_targets_kegg <- function(initial_targets, kegg_data) {
+  
+  extended_targets <- unlist(strsplit(initial_targets, ","))
+  
+  # Continue until no new targets are found
+  new_targets_found <- TRUE
+  while (new_targets_found) {
+    new_targets_found <- FALSE
+    
+    # Find interactions where the source gene is in our list of targets
+    new_targets <- kegg_data %>%
+      filter(genesymbol_source %in% extended_targets) %>%
+      pull(genesymbol_target)
+    
+    # Add unique new targets to our list
+    original_length <- length(extended_targets)
+    extended_targets <- unique(c(extended_targets, new_targets))
+    
+    if (length(extended_targets) > original_length) {
+      new_targets_found <- TRUE
+    }
+  }
+  return(extended_targets)
+}
+
+
+
+extend_targets_kegg_list <- function(initial_targets, list_kegg) {
+  # Loop to extend targets for each KEGG data frame
+  results <- list()
+  for (i in seq_along(list_kegg)) {
+    kegg_data <- list_kegg[[i]]
+    extended_targets <- extend_targets_kegg(initial_targets, kegg_data)
+    results[[names(list_kegg)[i]]] <- extended_targets
+  }
+  return(list("ext_kegg_targets" = paste(Reduce('union',results), collapse = ","), 
+              "ext_kegg_tar_cnt" = length(Reduce('union',results))))
+  return()
+}
+
+
+
+
+# Download the protein-protein interactions based on KEGG
+
+# download_KEGG_ixns <- function(drug_targets, pathway_ids) {
+#   # overlap <- rep(0,length(pathway_ids))
+#   list_kegg_pw <- list()
+#   nn <- c()
+#   for(i in 1:length(pathway_ids)) {
+#     print(pathway_ids[i])
+#     hsap <- kegg_pathway_download(pathway_ids[i], process = FALSE)
+#     kegg_nets <- kegg_process(hsap$entries, hsap$relations, simplify = FALSE)
+#     list_kegg_pw[[length(list_kegg_pw)+1]] <- kegg_nets
+#     print(length(intersect(drug_targets, union(kegg_nets$genesymbol_source, kegg_nets$genesymbol_target))))
+#     print(length(union(kegg_nets$genesymbol_source, kegg_nets$genesymbol_target)))
+#     if(length(union(kegg_nets$genesymbol_source, kegg_nets$genesymbol_target)) > 0)
+#       nn <- c(nn, pathway_ids[i])
+#   }
+#   names(list_kegg_pw) <- nn
+#   return(list_kegg_pw)
+# }
+
+download_KEGG_ixns <- function(pathway_ids){
+  list_kegg_pw <- list()
+  # summary_df <- data.frame()
+  
+  for(path in pathway_ids){
+    print(path)
+    kegg_net <- kegg_pathway_download(pathway_id = path, process = FALSE)
+    kegg_net <- kegg_process(kegg_net$entries, kegg_net$relations, simplify = FALSE)
+    
+    # summary_df <- rbind(summary_df, 
+    #                     data.frame("Pathway" = path,
+    #                                "Num_genes_in_pathway" = length(union(kegg_net$genesymbol_source, kegg_net$genesymbol_target)),
+    #                                "Num_drug_targets_in_pathway" = length(intersect(drug_targets, union(kegg_net$genesymbol_source, kegg_net$genesymbol_target)))
+    #                     ))
+    if(length(union(kegg_net$genesymbol_source, kegg_net$genesymbol_target)) > 0){
+      list_kegg_pw[[path]] <- kegg_net
+    }
+  }
+  
+  # cat("\n\nSummary of the downloaded KEGG interactions:\n")
+  # print(summary_df)
+  return(list_kegg_pw)
+}
