8cacad60b9794b83d60d3c08e1c7692c4633c2c0
ag1805x <ag1805x@outlook.in>
2023-11-22 11:16:25 +0200

    Feature size table update

--- Scripts/Summary_tables/Feature_size_table.R
+++ Scripts/Summary_tables/Feature_size_table.R
@@ -9,8 +9,15 @@ set.seed(5081)
 # Load libraries
 library(tidyverse)
 library(openxlsx)
+library(igraph)
+
 
 library_size <- list()
+
+# Read the network on which to execute RWR
+input_network <- readRDS("InputFiles/Networks/STRING_PPI_Net.rds")
+cat(paste0("\n\nInput network size:: vertices = ", vcount(input_network), ", edges = ", ecount(input_network), "\n\n"))
+
 
 
 # Compile efficacy libraries
@@ -19,8 +26,13 @@ diseases <- c("LungCancer", "BreastCancer", "ProstateCancer", "OvaryCancer", "Ki
 for(disease in diseases){
   enrichment_lib <- readRDS(paste0("InputFiles/Enrichment_analysis_libraries/Disease2Gene_", disease, "_lib.rds"))
   size <- as.data.frame(lengths(enrichment_lib))
-  colnames(size) <- "Size"
-  size <- rownames_to_column(size, "Description")
+  colnames(size) <- "all_size"
+  size_1 <- rownames_to_column(size, "Description")
+  enrichment_lib <- lapply(enrichment_lib, function(x){x[x %in% V(input_network)$name]})
+  size <- as.data.frame(lengths(enrichment_lib))
+  colnames(size) <- "inNet_size"
+  size_2 <- rownames_to_column(size, "Description")
+  size <- merge(size_1, size_2)
   library_size[[paste0("Efficacy_", disease)]] <- size
 }
 
@@ -29,8 +41,13 @@ for(disease in diseases){
 # Compile safety library
 enrichment_lib <- readRDS("InputFiles/Enrichment_analysis_libraries/curatedAdr2Gene_lib.rds")
 size <- as.data.frame(lengths(enrichment_lib))
-colnames(size) <- "Size"
-size <- rownames_to_column(size, "Description")
+colnames(size) <- "all_size"
+size_1 <- rownames_to_column(size, "Description")
+enrichment_lib <- lapply(enrichment_lib, function(x){x[x %in% V(input_network)$name]})
+size <- as.data.frame(lengths(enrichment_lib))
+colnames(size) <- "inNet_size"
+size_2 <- rownames_to_column(size, "Description")
+size <- merge(size_1, size_2)
 library_size[["Safety"]] <- size
 
 
@@ -38,8 +55,13 @@ library_size[["Safety"]] <- size
 # Compile KEGG library
 enrichment_lib <- readRDS(paste0("InputFiles/Enrichment_analysis_libraries/CHG_keggPath2Gene_lib.rds"))
 size <- as.data.frame(lengths(enrichment_lib))
-colnames(size) <- "Size"
-size <- rownames_to_column(size, "Description")
+colnames(size) <- "all_size"
+size_1 <- rownames_to_column(size, "Description")
+enrichment_lib <- lapply(enrichment_lib, function(x){x[x %in% V(input_network)$name]})
+size <- as.data.frame(lengths(enrichment_lib))
+colnames(size) <- "inNet_size"
+size_2 <- rownames_to_column(size, "Description")
+size <- merge(size_1, size_2)
 library_size[["KEGG"]] <- size
 
 
@@ -48,8 +70,13 @@ library_size[["KEGG"]] <- size
 enrichment_lib <- readRDS(paste0("InputFiles/Enrichment_analysis_libraries/SMPDb_Pathway2Gene_lib.rds"))
 enrichment_lib <- enrichment_lib$`Drug Metabolism`
 size <- as.data.frame(lengths(enrichment_lib))
-colnames(size) <- "Size"
-size <- rownames_to_column(size, "Description")
+colnames(size) <- "all_size"
+size_1 <- rownames_to_column(size, "Description")
+enrichment_lib <- lapply(enrichment_lib, function(x){x[x %in% V(input_network)$name]})
+size <- as.data.frame(lengths(enrichment_lib))
+colnames(size) <- "inNet_size"
+size_2 <- rownames_to_column(size, "Description")
+size <- merge(size_1, size_2)
 library_size[["SMPDB_DrugMetabolism"]] <- size
 
 
@@ -58,8 +85,13 @@ library_size[["SMPDB_DrugMetabolism"]] <- size
 enrichment_lib <- readRDS(paste0("InputFiles/Enrichment_analysis_libraries/SMPDb_Pathway2Gene_lib.rds"))
 enrichment_lib <- enrichment_lib$`Drug Action`
 size <- as.data.frame(lengths(enrichment_lib))
-colnames(size) <- "Size"
-size <- rownames_to_column(size, "Description")
+colnames(size) <- "all_size"
+size_1 <- rownames_to_column(size, "Description")
+enrichment_lib <- lapply(enrichment_lib, function(x){x[x %in% V(input_network)$name]})
+size <- as.data.frame(lengths(enrichment_lib))
+colnames(size) <- "inNet_size"
+size_2 <- rownames_to_column(size, "Description")
+size <- merge(size_1, size_2)
 library_size[["SMPDB_DrugAction"]] <- size
 
 
@@ -67,8 +99,13 @@ library_size[["SMPDB_DrugAction"]] <- size
 # Compile miscelleneous library
 enrichment_lib <- readRDS(paste0("InputFiles/Enrichment_analysis_libraries/miscellaneous_gene_lib.rds"))
 size <- as.data.frame(lengths(enrichment_lib))
-colnames(size) <- "Size"
-size <- rownames_to_column(size, "Description")
+colnames(size) <- "all_size"
+size_1 <- rownames_to_column(size, "Description")
+enrichment_lib <- lapply(enrichment_lib, function(x){x[x %in% V(input_network)$name]})
+size <- as.data.frame(lengths(enrichment_lib))
+colnames(size) <- "inNet_size"
+size_2 <- rownames_to_column(size, "Description")
+size <- merge(size_1, size_2)
 library_size[["miscellaneous"]] <- size
 
 
@@ -79,24 +116,38 @@ if(!dir.exists("OutputFiles/Plots/")){
   dir.create("OutputFiles/Plots/", recursive = TRUE)
 }
 tiff("OutputFiles/Plots/Enrichment_library_size.tiff",
-     width = 15, height = 5,
+     width = 20, height = 10,
      units = "cm", compression = "lzw", res = 1200)
 
 plot_data <- bind_rows(library_size, .id = "Source")
 
-plot_data$Size[plot_data$Size >= 500] <- 500
+plot_data$all_size[plot_data$all_size >= 500] <- 500
+plot_data$inNet_size[plot_data$inNet_size >= 500] <- 500
 
 plot_data$Source <- factor(plot_data$Source, 
                            levels = c("Efficacy_LungCancer", "Efficacy_BreastCancer", "Efficacy_ProstateCancer", 
                                       "Efficacy_OvaryCancer", "Efficacy_KidneyCancer", "Efficacy_SkinCancer", 
                                       "Safety", "KEGG", "SMPDB_DrugMetabolism", "SMPDB_DrugAction", "miscellaneous"))
 
+
+
 plot_data_summary <- plot_data %>% group_by(Source) %>% summarise(n = n())
 
+
+
+plot_data <- pivot_longer(plot_data, 
+                          cols = c("all_size", "inNet_size"), 
+                          names_to = "type", 
+                          values_to = "size")
+
+
 ggplot() +
-  geom_violin(data = plot_data, 
-              aes(x = Source, y = Size), 
-              fill = "grey", lwd = 0.1) +
+  geom_boxplot(data = plot_data, 
+               aes(x = Source, y = size, fill = type), 
+               width = 0.5, 
+               lwd = 0.1,
+               outlier.shape = 3,
+               outlier.size = 0.5) +
   geom_text(data = plot_data_summary,
             aes(x = Source, y = Inf, label = n),
             color = "red", size = 2, vjust = 2) +
@@ -105,9 +156,18 @@ ggplot() +
         panel.spacing = unit(0.1, "cm"),
         text = element_text(size = 6),
         axis.text.x = element_text(size = 4, angle = 45, vjust = 1, hjust = 1),
-        axis.ticks = element_line(colour = "black", linewidth = 0.2)) +
+        axis.ticks = element_line(colour = "black", linewidth = 0.2),
+        legend.position = "bottom",
+        legend.key = element_blank(),
+        legend.key.size = unit(0.5, 'cm'),
+        legend.title = element_text(size = 3),
+        legend.text = element_text(size = 3),
+        legend.margin = margin(1,1,1,1),
+        legend.box.spacing = unit(0.1, 'cm'),
+        legend.box.background = element_rect(colour = "black", linewidth = 0.25)) +
   labs(x = "Library",
-       y = "Library size")
+       y = "Library size",
+       fill = "Type")
 
 dev.off()
 
