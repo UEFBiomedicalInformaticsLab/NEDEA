d8cbccfaed210b2e5c13f17abb7b5af464d38feb
ag1805x <ag1805x@outlook.in>
2024-11-11 11:40:13 +0200

    summarise drug counts

--- Scripts/Summary_tables/Summary_validNdenovo_drugComb_count.R
+++ Scripts/Summary_tables/Summary_validNdenovo_drugComb_count.R
@@ -9,7 +9,11 @@ set.seed(5081)
 library(tidyverse)
 
 
-# Validation data
+#####
+
+
+# Read the Validation data
+
 validation_drugCombs <- list()
 
 for(disease in c("BreastCancer", "KidneyCancer", "LungCancer", "OvaryCancer", "ProstateCancer", "SkinCancer")){
@@ -22,12 +26,20 @@ for(disease in c("BreastCancer", "KidneyCancer", "LungCancer", "OvaryCancer", "P
     validation_drugCombs[["Validation1a"]][[disease]] <- readRDS(paste0("InputFiles/Validation_data_1a/drugCombs_validation1a_", disease, ".rds"))
   }
   
+  if(file.exists(paste0("InputFiles/Validation_data_1b/drugCombs_validation1b_", disease, ".rds"))){
+    validation_drugCombs[["Validation1b"]][[disease]] <- readRDS(paste0("InputFiles/Validation_data_1b/drugCombs_validation1b_", disease, ".rds"))
+  }
+  
   if(file.exists(paste0("InputFiles/Validation_data_2/drugCombs_validation2_", disease, ".rds"))){
     validation_drugCombs[["Validation2"]][[disease]] <- readRDS(paste0("InputFiles/Validation_data_2/drugCombs_validation2_", disease, ".rds"))
   }
   
   if(file.exists(paste0("InputFiles/Validation_data_2a/drugCombs_validation2a_", disease, ".rds"))){
     validation_drugCombs[["Validation2a"]][[disease]] <- readRDS(paste0("InputFiles/Validation_data_2a/drugCombs_validation2a_", disease, ".rds"))
+  }
+  
+  if(file.exists(paste0("InputFiles/Validation_data_2b/drugCombs_validation2b_", disease, ".rds"))){
+    validation_drugCombs[["Validation2b"]][[disease]] <- readRDS(paste0("InputFiles/Validation_data_2b/drugCombs_validation2b_", disease, ".rds"))
   }
   
   if(file.exists(paste0("InputFiles/Validation_data_3/drugCombs_validation3_", disease, ".rds"))){
@@ -38,8 +50,36 @@ for(disease in c("BreastCancer", "KidneyCancer", "LungCancer", "OvaryCancer", "P
     validation_drugCombs[["Validation3a"]][[disease]] <- readRDS(paste0("InputFiles/Validation_data_3a/drugCombs_validation3a_", disease, ".rds"))
   }
   
+  if(file.exists(paste0("InputFiles/Validation_data_3b/drugCombs_validation3b_", disease, ".rds"))){
+    validation_drugCombs[["Validation3b"]][[disease]] <- readRDS(paste0("InputFiles/Validation_data_3b/drugCombs_validation3b_", disease, ".rds"))
+  }
+  
   if(file.exists(paste0("InputFiles/Validation_data_4/drugCombs_validation4_", disease, ".rds"))){
     validation_drugCombs[["Validation4"]][[disease]] <- readRDS(paste0("InputFiles/Validation_data_4/drugCombs_validation4_", disease, ".rds"))
+  }
+  
+  if(file.exists(paste0("InputFiles/Validation_data_6/drugCombs_validation6_", disease, ".rds"))){
+    validation_drugCombs[["Validation6"]][[disease]] <- readRDS(paste0("InputFiles/Validation_data_6/drugCombs_validation6_", disease, ".rds"))
+  }
+  
+  if(file.exists(paste0("InputFiles/Validation_data_6a/drugCombs_validation6a_", disease, ".rds"))){
+    validation_drugCombs[["Validation6a"]][[disease]] <- readRDS(paste0("InputFiles/Validation_data_6a/drugCombs_validation6a_", disease, ".rds"))
+  }
+  
+  if(file.exists(paste0("InputFiles/Validation_data_6b/drugCombs_validation6b_", disease, ".rds"))){
+    validation_drugCombs[["Validation6b"]][[disease]] <- readRDS(paste0("InputFiles/Validation_data_6b/drugCombs_validation6b_", disease, ".rds"))
+  }
+  
+  if(file.exists(paste0("InputFiles/Validation_data_7/drugCombs_validation7_", disease, ".rds"))){
+    validation_drugCombs[["Validation7"]][[disease]] <- readRDS(paste0("InputFiles/Validation_data_7/drugCombs_validation7_", disease, ".rds"))
+  }
+  
+  if(file.exists(paste0("InputFiles/Validation_data_7a/drugCombs_validation7a_", disease, ".rds"))){
+    validation_drugCombs[["Validation7a"]][[disease]] <- readRDS(paste0("InputFiles/Validation_data_7a/drugCombs_validation7a_", disease, ".rds"))
+  }
+  
+  if(file.exists(paste0("InputFiles/Validation_data_7b/drugCombs_validation7b_", disease, ".rds"))){
+    validation_drugCombs[["Validation7b"]][[disease]] <- readRDS(paste0("InputFiles/Validation_data_7b/drugCombs_validation7b_", disease, ".rds"))
   }
   
 }
@@ -51,8 +91,10 @@ validation_drugCombs <- separate(data = validation_drugCombs, col = "Disease", i
 validation_drugCombs <- pivot_wider(validation_drugCombs, names_from = Var1, values_from = Freq)
 
 
+#####
 
-# De Novo data
+
+# Read the de Novo data
 
 denovo_drugCombs <- list()
 
@@ -69,21 +111,26 @@ denovo_drugCombs <- lapply(denovo_drugCombs, function(x){ data.frame("Unk" = nro
 denovo_drugCombs <- bind_rows(denovo_drugCombs, .id = "Disease")
 denovo_drugCombs <- separate(data = denovo_drugCombs, col = "Disease", into = c("DataSet", "Disease"))
 
+
+#####
+
+
+# Merge data and save counts
+
 drugCombs <- bind_rows(validation_drugCombs, denovo_drugCombs)
-
-
-
 
 drugCombs <- pivot_wider(data = drugCombs,
                          names_from = c("Disease"), 
                          values_from = c("Eff", "Adv", "Unk"), 
                          names_glue = "{Disease}_{.value}")
 
-
 drugCombs <- column_to_rownames(drugCombs, "DataSet")
 drugCombs <- drugCombs[,sort(colnames(drugCombs))]
 
 write.csv(drugCombs, "OutputFiles/Tables/Summary_validNdenovo_drugComb_count.csv", row.names = TRUE)
+
+
+#####
 
 
 print(warnings())
